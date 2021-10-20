07b064eb7aec380a7677350726542b2094b01612
Spidermoy10 <moisestejeda02@gmail.com>
2021-10-20 07:43:36 -0500

    HolaPepe

--- django/hola/principal/urls.py
+++ django/hola/principal/urls.py
@@ -3,6 +3,6 @@ from.import views
 
 urlpatterns=[
     path("",views.holaDjango,name="holaDjango"),
-    path("pepe",views.pepe,name="HolaPepe"),
+    path("",views.pepe,name="HolaPepe"),
     path('<str:nombre>',views.holaTu, name='holaTu')
 ]
